78e25f507caf196d8d96dd49e5acdef59c3cf53b
Anthony Lopez <anthonybeconscious@gmail.com>
2020-02-04 12:18:34 -0600

    updated to production

--- knexfile.js
+++ knexfile.js
@@ -17,4 +17,23 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/med_cab.db3',//change once the db has been solified from ds
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
